8e44eada0d862d1ae54b5db7989b14fe53d20437
kotamadeo <kotamadeo@gmail.com>
2022-10-19 13:52:34 +0300

    merging

--- src/main/java/com/gmail/at/kotamadeo/phonebook/Phonebook.java
+++ src/main/java/com/gmail/at/kotamadeo/phonebook/Phonebook.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Getter
@@ -16,36 +14,21 @@ public class Phonebook {
     private String name;
     private final Map<String, String> numbers = new HashMap<>();
 
-    public int putContactToPhonebook(String name, String number) {
-        numbers.putIfAbsent(name, number);
-        return numbers.size();
+    public int putNumberToPhoneBook(String name, String number) {
+        return 0;
     }
 
     public String findByNumber(String number) {
-        return numbers.entrySet()
-                .stream()
-                .filter(entry -> number.equals(entry.getValue()))
-                .map(Map.Entry::getKey)
-                .findFirst().orElse(null);
+        return null;
     }
 
     public String findByName(String name) {
-        return numbers.entrySet()
-                .stream()
-                .filter(entry -> name.equals(entry.getKey()))
-                .map(Map.Entry::getValue)
-                .findFirst().orElse(null);
+        return null;
     }
 
     public String printAllNames() {
-        return numbers.entrySet()
-                .stream()
-                .map(Objects::toString)
-                .collect(Collectors.joining(", "));
+        return null;
     }
 
-    @Override
-    public String toString() {
-       return String.format("%s:%n%s", name, printAllNames());
-    }
+
 }
